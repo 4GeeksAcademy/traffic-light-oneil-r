bcd78a615e6d93480d50b369e6e602a0ce8c608b
Oneil-R <167453370+Oneil-R@users.noreply.github.com>
2024-07-11 00:23:16 +0000

    complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState('red');
+	const [colors, setColors] = useState(['red', 'yellow', 'green']);
+  
+	const handleColorClick = (selectedColor) => {
+	  setColor(selectedColor);
+	};
+  
+	const cycleColors = () => {
+	  const currentIndex = colors.indexOf(color);
+	  const nextIndex = (currentIndex + 1) % colors.length;
+	  setColor(colors[nextIndex]);
+	};
+  
+	const addPurpleColor = () => {
+	  if (!colors.includes('purple')) {
+		setColors([...colors, 'purple']);
+	  }
+	};
+  
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	  <div className="home-container">
+		<div className="traffic-light-container">
+		  <div className="traffic-light">
+			{colors.map((col) => (
+			  <div
+				key={col}
+				className={`light ${col} ${color === col ? 'glow' : ''}`}
+				onClick={() => handleColorClick(col)}
+			  ></div>
+			))}
+		  </div>
+		  <button className="cycle-button" onClick={cycleColors}>
+			Cycle Colors
+		  </button>
+		  <button className="add-purple-button" onClick={addPurpleColor}>
+			Add Purple
+		  </button>
 		</div>
+	  </div>
 	);
-};
+  };
 
 export default Home;
